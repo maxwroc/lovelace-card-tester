695b707f335ce9390d2e335d4634a0bc5fbd0d28
maxwroc <max.chodorowski@gmail.com>
2020-07-15 14:39:41 +0100

    Added code editor

--- card-controller.js
+++ card-controller.js
@@ -8,11 +8,32 @@ class CardController {
 
     config = null;
 
+    editor = {
+        config: null,
+        state: null
+    }
+
+    cardLoaded = false;
+
     constructor() {
         this.addCardSource();
-        this.updateConfig();
 
-        $("#cardConfig").on("input", () => this.updateConfig());
+        this.editor.config = new CodeFlask("#configEditor", { language: "yaml", lineNumbers: true });
+        this.editor.config.addLanguage("yaml", CodeLangs.languages["yaml"]);
+        // trigger re-rendering
+        this.editor.config.updateCode(this.editor.config.getCode());
+
+        this.editor.state = new CodeFlask("#haStateEditor", { language: "json", lineNumbers: true });
+        this.editor.state.addLanguage("json", CodeLangs.languages["json"]);
+        // trigger re-rendering
+        this.editor.state.updateCode(this.editor.state.getCode());
+
+
+        this.editor.config.onUpdate(() => this.updateConfig());
+        this.editor.state.onUpdate(() => this.updateState());
+
+        // start processing config
+        this.updateConfig();
     }
 
     addCardSource() {
@@ -23,16 +44,25 @@ class CardController {
     }
 
     loadCard() {
+        this.cardLoaded = false;
+
         customElements.whenDefined(this.cardType).then(() => {
             this.card = document.createElement(this.cardType);
             this.card.setConfig(this.config);
             $("#cardContainer").html("").append(this.card);
+
+            console.log("loaded");
+            this.cardLoaded = true;
+
+            // triggering config update
+            this.updateConfig();
         });
     }
 
     updateConfig() {
+
         try {
-            this.config = jsyaml.load($("#cardConfig").val());
+            this.config = jsyaml.load(this.editor.config.getCode());
 
             if (!this.config.type) {
                 throw new Error("Missing propery 'type' in card configuration");
@@ -44,10 +74,34 @@ class CardController {
                 return;
             }
 
+            if (!this.cardLoaded) {
+                return;
+            }
+
             this.card.setConfig(this.config);
+
+            this.updateState();
         }
         catch (e) {
             console.log(e);
+        }
+    }
+
+    updateState() {
+        if (!this.cardLoaded) {
+            return;
+        }
+
+        try {
+            const hass = {
+                states: JSON.parse(this.editor.state.getCode()),
+                localize: key => `[${key}]`
+            }
+
+            this.card.hass = hass;
+        }
+        catch (e) {
+            console.log(e)
         }
     }
 }
